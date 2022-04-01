0101a8ea511e3569152fafaf5d9ad9d953fba52a
maryalenn <maryachuong@gmail.com>
2022-03-31 21:30:32 -0700

    Issue 1, Tasks 5-15 - Submit functionality and storing inputs to local storage

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 /* global data */
 /* exported data */
-
-// the input of  #photo-url;
+// the form
+var $entryForm = document.getElementById('entry-form');
+var $entryImage = document.querySelector('.placeholder');
+// the form inputs
 var $inputImageURL = document.querySelector('#photo-url');
+var $inputTitle = document.querySelector('#title');
+var $inputNotes = document.querySelector('#notes');
 
+// Event listener to update img src with input values
 $inputImageURL.addEventListener('change', updateImage);
 
 // function to insert the value of the input URL into the placeholder image src)
 function updateImage(event) {
-  // identify the placeholder image
-  var $entryImage = document.querySelector('.placeholder');
-
   // update the class of the placeholder image to .entry-image. Remove placeholder class & alt text
   $entryImage.classList.add('entry-image');
-  $entryImage.classList.remove('placeholder');
   $entryImage.alt = '';
   $entryImage.src = event.target.value;
+  $inputImageURL = event.target.value;
   // update the src attribute of the image to the $inputImageURL value
   return $entryImage;
 }
-
 // event listener for submit event on journal entry form
-
-// function to put forn inpu values into a new object
+$entryForm.addEventListener('submit', saveEntry);
+function saveEntry(event) {
+  event.preventDefault();
+  $inputNotes = $entryForm.elements.notes.value;
+  $inputTitle = $entryForm.elements.title.value;
+  data.entries.push({
+    title: $inputTitle,
+    photoURL: $inputImageURL,
+    notes: $inputNotes
+  });
+  data.nextEntryId++;
+  $entryImage.src = 'images/placeholder-image-square.jpg';
+  // console.log(data);
+  $entryForm.reset();
+}
