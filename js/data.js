2ff7e989258e9c185ed37f836630197e06e97e9f
Marya Chuong <97073517+maryalenn@users.noreply.github.com>
2022-04-10 16:14:42 -0700

    Revert "Completed Issue 2 view entries"

--- js/data.js
+++ js/data.js
@@ -6,17 +6,3 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-
-// Storing Previous Entries
-var previousEntriesJSON = localStorage.getItem('javascript-local-storage');
-// console.log('previous entries: ', previousEntriesJSON);
-// function to put form input values into a new object, add nextEntryId
-
-window.addEventListener('beforeunload', function (event) {
-  var entriesJSON = JSON.stringify(data);
-  localStorage.setItem('javascript-local-storage', entriesJSON);
-});
-
-if (previousEntriesJSON != null) {
-  data = JSON.parse(previousEntriesJSON);
-}
